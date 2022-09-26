95e1682772c7811ea9ff782ef1f5a3a51d5b11ff
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-26 18:11:24 +0300

    Completed task: T01.ReverseString

---
+++ Stacks-and-queues-lab/Stacks-and-queues-lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_and_queues_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var ch in text)
+            {
+                stack.Push(ch);
+            }
+
+            Console.WriteLine(String.Join("", stack));
+        }
+    }
+}
+
