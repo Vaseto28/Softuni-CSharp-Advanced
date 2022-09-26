33527d3bb1f9b9a6599d3460ce7f6b1a0cb49de3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-26 18:12:23 +0300

    Completed task: T04.MatchingBrackets

---
+++ Stacks-and-queues-lab/T04.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            Stack<int> stack = new Stack<int>(); 
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                char currChar = inputLine[i];
+                if (currChar == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (currChar == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string currExpression = inputLine.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(currExpression);
+                }
+            }
+        }
+    }
+}
+
