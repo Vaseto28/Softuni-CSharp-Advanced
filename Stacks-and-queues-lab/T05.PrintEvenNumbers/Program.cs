e7e041cb53ee8cca8a6262390caba549fa4f8bd4
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-27 11:37:21 +0300

    Completed task: T05.PrintEvenNumbers

---
+++ Stacks-and-queues-lab/T05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            foreach (var num in numbers)
+            {
+                if (num%2==0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
+
