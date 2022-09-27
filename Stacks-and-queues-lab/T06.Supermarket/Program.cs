c466626a8c6080af51116751a7bf519fc63fd108
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-27 11:37:36 +0300

    Completed task: T06.Supermarket

---
+++ Stacks-and-queues-lab/T06.Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Paid")
+                {
+                    Console.WriteLine(String.Join(Environment.NewLine, queue));
+                    queue.Clear();
+                }
+                else if (command == "End")
+                {
+                    Console.WriteLine($"{queue.Count} people remaining.");
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
+
