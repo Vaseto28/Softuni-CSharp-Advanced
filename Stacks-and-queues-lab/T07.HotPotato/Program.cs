01f47eca9e151978b634151973cdf5c34bfc1043
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-27 11:37:53 +0300

    Completed task: T07.HotPotato

---
+++ Stacks-and-queues-lab/T07.HotPotato/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> children = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            int n = int.Parse(Console.ReadLine());
+
+            while (children.Count>1)
+            {
+                for (int i = 1; i < n; i++)
+                { 
+                    children.Enqueue(children.Dequeue());
+                }
+                Console.WriteLine($"Removed {children.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
+
