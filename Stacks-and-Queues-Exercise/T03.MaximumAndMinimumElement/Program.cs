cb618fb3bc757ce6bbb795f96f793ac3b03d7dde
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-28 12:34:55 +0300

    Completed task: T03.MaximumAndMinimumElement

---
+++ Stacks-and-Queues-Exercise/T03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class MaximumAndMinimumElement
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Stack<int> stack = new Stack<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] command = Console.ReadLine().Split();
+            int numberOfCommand = int.Parse(command[0]);
+            if (numberOfCommand == 1)
+            {
+                stack.Push(int.Parse(command[1]));
+            }
+            else if (numberOfCommand == 2)
+            {
+                if (stack.Count <= 0)
+                {
+                    continue;
+                }
+                stack.Pop();
+            }
+            else if (numberOfCommand == 3)
+            {
+                if (stack.Count <= 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(stack.Max());
+            }
+            else if (numberOfCommand == 4)
+            {
+                if (stack.Count <= 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(stack.Min());
+            }
+        }
+
+        Console.WriteLine(String.Join(", ", stack));
+    }
+}
