7b08501eae7fe83a0d11e4477f5bcbd4bd9bfb9f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-28 12:35:14 +0300

    Completed task: T04.FastFood

---
+++ Stacks-and-Queues-Exercise/T04.FastFood/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class FastFood
+{
+    static void Main()
+    {
+        int availableFood = int.Parse(Console.ReadLine());
+
+        Queue<int> orders = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        Console.WriteLine(orders.Max());
+        while (orders.Count > 0)
+        {
+            int currOrder = orders.Peek();
+            if (availableFood >= currOrder)
+            {
+                availableFood -= currOrder;
+                orders.Dequeue();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (orders.Count <= 0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        else
+        {
+            Console.WriteLine($"Orders left: {String.Join(" ", orders)}");
+        }
+    }
+}
