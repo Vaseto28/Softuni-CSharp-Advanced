0b73f663261748bf21b2fb8d5ca64c8c3887fa0c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-28 12:33:11 +0300

    Completed task: T02.BasicQueueOperation

---
+++ Stacks-and-Queues-Exercise/T02.BasicQueueOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class BasicStackOperations
+{
+    static void Main()
+    {
+        int[] @params = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = @params[0];
+        int s = @params[1];
+        int x = @params[2];
+
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Queue<int> queue = new Queue<int>();
+        for (int i = 0; i < n; i++)
+        {
+            queue.Enqueue(numbers[i]);
+        }
+
+        for (int i = 0; i < s; i++)
+        {
+            queue.Dequeue();
+        }
+
+        if (queue.Count == 0)
+        {
+            Console.WriteLine(0);
+        }
+        else if (queue.Contains(x))
+        {
+            Console.WriteLine("true");
+        }
+        else if (!queue.Contains(x))
+        {
+            Console.WriteLine(queue.Min());
+        }
+    }
+}
