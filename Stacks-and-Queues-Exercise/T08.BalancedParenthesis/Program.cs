8dedc3ff7e99de26671e3d8f036aeea05980b90a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-29 11:31:53 +0300

    Completed task: T08.BalancedParenthesis

---
+++ Stacks-and-Queues-Exercise/T08.BalancedParenthesis/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class BalancedParenthesis
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Stack<char> parenthesis = new Stack<char>();
+
+        foreach (char ch in input)
+        {
+            if (parenthesis.Count != 0)
+            {
+                char secondCh = parenthesis.Peek();
+                if (secondCh == '(' && ch == ')')
+                {
+                    parenthesis.Pop();
+                    continue;
+                }
+
+                if (secondCh == '[' && ch == ']')
+                {
+                    parenthesis.Pop();
+                    continue;
+                }
+
+                if (secondCh == '{' && ch == '}')
+                {
+                    parenthesis.Pop();
+                    continue;
+                }
+            }
+
+            parenthesis.Push(ch);
+        }
+
+        if (parenthesis.Count <= 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
