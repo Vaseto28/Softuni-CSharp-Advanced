e0b58e1f12e08945866a04154487b116929e3280
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-29 11:31:29 +0300

    Completed task: T07.TruckTour

---
+++ Stacks-and-Queues-Exercise/T07.TruckTour/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class TruckTour
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var petrol = new Queue<int>();
+        var distance = new Queue<int>();
+        int[] input;
+        for (int i = 0; i < n; i++)
+        {
+            input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            petrol.Enqueue(input[0]);
+            distance.Enqueue(input[1]);
+        }
+
+        int currentFuel;
+        for (int i = 0; i < n; i++)
+        {
+            currentFuel = petrol.Peek();
+            for (int x = 0; x < n; x++)
+            {
+                if (distance.Peek() <= currentFuel)
+                {
+                    currentFuel -= distance.Peek();
+                    if (x == n - 1)
+                    {
+                        Console.WriteLine(i);
+                        return;
+                    }
+                }
+                else
+                {
+                    for (int y = x; y < n; y++)
+                    {
+                        petrol.Enqueue(petrol.Dequeue());
+                        distance.Enqueue(distance.Dequeue());
+                    }
+                    break;
+                }
+                petrol.Enqueue(petrol.Dequeue());
+                distance.Enqueue(distance.Dequeue());
+                currentFuel += petrol.Peek();
+            }
+            petrol.Enqueue(petrol.Dequeue());
+            distance.Enqueue(distance.Dequeue());
+        }
+    }
+}
