09597ba108d3bff6b1698ee8ff42dde5e72e7d94
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-30 12:22:27 +0300

    Completed task: T01.SumMatrixElements

---
+++ Multidimentional arrays - Lab/T01.SumMatrixElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+class SumMatrixElement
+{
+    static void Main()
+    {
+        int[] matrixSize = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int rows = matrixSize[0];
+        int cols = matrixSize[1];
+        int[,] matrix = new int[rows, cols];
+
+        ReadMatrix(rows, matrix);
+
+        int sum = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                sum += matrix[row, col];
+            }
+        }
+
+        Console.WriteLine(rows);
+        Console.WriteLine(cols);
+        Console.WriteLine(sum);
+    }
+
+    private static void ReadMatrix(int rows, int[,] matrix)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            int[] matrixElements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int col = 0; col < matrixElements.Length; col++)
+            {
+                matrix[row, col] = matrixElements[col];
+            }
+        }
+    }
+}
