cadf38ac65951d6d33f438a0c9f6a5abeb37dc21
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-09-30 12:23:13 +0300

    Completed task: T03.PrimaryDiagonal

---
+++ Multidimentional arrays - Lab/T03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class PrimaryDiagonal
+{
+    static void Main()
+    {
+        int dimention = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[dimention, dimention];
+
+        ReadMatrix(dimention, matrix);
+
+        int sum = 0;
+        for (int row = 0; row < dimention; row++)
+        {
+            for (int col = 0; col < dimention; col++)
+            {
+                if (row == col)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static void ReadMatrix(int dimention, int[,] matrix)
+    {
+        for (int row = 0; row < dimention; row++)
+        {
+            int[] matrixElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int col = 0; col < matrixElements.Length; col++)
+            {
+                matrix[row, col] = matrixElements[col];
+            }
+        }
+    }
+}
