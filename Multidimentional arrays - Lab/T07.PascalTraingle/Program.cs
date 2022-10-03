7c18fe83abe60b2b4570fc0341c837896746ca6c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-03 23:13:34 +0300

    Completed task: T07.PascalTriangle

---
+++ Multidimentional arrays - Lab/T07.PascalTraingle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class PascalTraingle
+{
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        long[][] pascalTraingle = new long[rows][];
+
+        for (int row = 0; row < rows; row++)
+        {
+            pascalTraingle[row] = new long[row + 1];
+            pascalTraingle[row][0] = 1;
+
+            for (int col = 1; col < row; col++)
+            {
+                pascalTraingle[row][col] = pascalTraingle[row - 1][col - 1] + pascalTraingle[row - 1][col];
+            }
+
+            pascalTraingle[row][row] = 1;
+        }
+
+        PrintJaggedArray(rows, pascalTraingle);
+    }
+
+    private static void PrintJaggedArray(int rows, long[][] jaggedArray)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            Console.WriteLine(String.Join(" ", jaggedArray[row]));
+        }
+    }
+}
