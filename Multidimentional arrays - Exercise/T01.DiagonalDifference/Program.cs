e12b83ba4b476a52448644a269e81c1aa9d60926
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-04 09:08:02 +0300

    Completed task: T01.DiagonalDifference

---
+++ Multidimentional arrays - Exercise/T01.DiagonalDifference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+class DiagonalDifference
+{
+    static void Main()
+    {
+        int dimention = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[dimention, dimention];
+
+        ReadMatrix(dimention, matrix);
+
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+        for (int row = 0; row < dimention; row++)
+        {
+            for (int col = 0; col < dimention; col++)
+            {
+                if (row == col)
+                {
+                    primaryDiagonalSum += matrix[row, col];
+                }
+
+                if (row + col == dimention - 1)
+                {
+                    secondaryDiagonalSum += matrix[row, col];
+                }
+            }
+        }
+
+        Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+    }
+
+    private static void ReadMatrix(int dimention, int[,] matrix)
+    {
+        for (int row = 0; row < dimention; row++)
+        {
+            int[] matrixElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int col = 0; col < matrixElements.Length; col++)
+            {
+                matrix[row, col] = matrixElements[col];
+            }
+        }
+    }
+}
