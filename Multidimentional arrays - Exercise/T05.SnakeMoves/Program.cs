c9aa305d877a74c3344019697df9740248f2a39e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-04 09:09:34 +0300

    Completed task: T05.SnakeMoves

---
+++ Multidimentional arrays - Exercise/T05.SnakeMoves/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+class SnakeMoves
+{
+    static void Main()
+    {
+        int[] dimentions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int rows = dimentions[0];
+        int cols = dimentions[1];
+
+        char[,] matrix = new char[rows, cols];
+
+        char[] snake = Console.ReadLine().ToCharArray();
+
+        int index = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            if (row % 2 == 0)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = snake[index];
+                    index++;
+
+                    if (index == snake.Length)
+                    {
+                        index = 0;
+                    }
+                }
+            }
+            else
+            {
+                for (int col = cols - 1; col >= 0; col--)
+                {
+                    matrix[row, col] = snake[index];
+                    index++;
+
+                    if (index == snake.Length)
+                    {
+                        index = 0;
+                    }
+                }
+            }
+        }
+
+        PrintMatrix(rows, cols, matrix);
+    }
+
+    private static void PrintMatrix(int rows, int cols, char[,] matrix)
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                Console.Write(matrix[row, col]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
