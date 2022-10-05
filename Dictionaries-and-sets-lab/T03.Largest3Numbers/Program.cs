9579f2aa564755d2a8f68d16bd93568ff2acf4f3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-05 12:34:20 +0300

    Completed task: T03.Largest3Numbers

---
+++ Dictionaries-and-sets-lab/T03.Largest3Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Largest3Numbers
+{
+    public static void Main()
+    {
+        List<int> nums = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .OrderByDescending(x => x)
+            .Take(3)
+            .ToList();
+        Console.WriteLine(String.Join(" ", nums));
+    }
+}
