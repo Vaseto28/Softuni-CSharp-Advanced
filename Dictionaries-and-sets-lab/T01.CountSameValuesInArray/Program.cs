7ccdc1a27426bac6ba377f10288f720dac5bfa96
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-05 12:33:17 +0300

    Completed task: T01.CountSameValuesInArray

---
+++ Dictionaries-and-sets-lab/T01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class CountSameValuesInArray
+{
+    public static void Main()
+    {
+        string[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string, int> occurances = new Dictionary<string, int>();
+
+        foreach (var num in nums)
+        {
+            if (!occurances.ContainsKey(num))
+            {
+                occurances.Add(num, 0);
+            }
+            occurances[num]++;
+        }
+
+        foreach (var kvp in occurances)
+        {
+            Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+        }
+    }
+}
