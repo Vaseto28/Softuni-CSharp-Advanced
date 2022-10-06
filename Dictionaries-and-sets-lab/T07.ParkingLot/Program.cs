4fd0ce497926e91e7fa5a46df98d8c1a9afefade
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-06 10:21:50 +0300

    Completed task: T07.ParkingLot

---
+++ Dictionaries-and-sets-lab/T07.ParkingLot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class ParkingLot
+{
+    public static void Main()
+    {
+        HashSet<string> plates = new HashSet<string>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
+        {
+            string[] info = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            string action = info[0];
+            string plate = info[1];
+
+            if (action == "IN")
+            {
+                plates.Add(plate);
+            }
+            else if (action == "OUT")
+            {
+                plates.Remove(plate);
+            }
+        }
+
+        if (plates.Count > 0)
+            Console.WriteLine(String.Join(Environment.NewLine, plates));
+        else
+            Console.WriteLine("Parking Lot is Empty");
+    }
+}
