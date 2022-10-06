b9d81b7fa37e5b3a428aa186b3b98220b3e2053c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-06 10:24:36 +0300

    Completed task: T08.SoftUniParty

---
+++ Dictionaries-and-sets-lab/T08.SoftUniParty/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class SoftUniParty
+{
+    public static void Main()
+    {
+        HashSet<string> normalGuests = new HashSet<string>();
+        HashSet<string> vipGuests = new HashSet<string>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "PARTY")
+        {
+            string currNumber = input;
+            if (char.IsDigit(currNumber[0]))
+            {
+                vipGuests.Add(currNumber);
+            }
+            else
+            {
+                normalGuests.Add(currNumber);
+            }
+        }
+
+        string guest;
+        while ((guest = Console.ReadLine()) != "END")
+        {
+            if (char.IsDigit(guest[0]))
+            {
+                vipGuests.Remove(guest);
+            }
+            else
+            {
+                normalGuests.Remove(guest);
+            }
+        }
+
+        Console.WriteLine(vipGuests.Count + normalGuests.Count);
+        if (vipGuests.Count > 0)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, vipGuests));
+        }
+
+        if (normalGuests.Count > 0)
+        {
+            Console.WriteLine(String.Join(Environment.NewLine, normalGuests));
+        }
+    }
+}
