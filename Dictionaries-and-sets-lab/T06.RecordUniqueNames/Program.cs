860f4930210086c96bed50ebb6423b64ea83d95f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-06 10:21:31 +0300

    Completed task: T06.RecordUniqueNames

---
+++ Dictionaries-and-sets-lab/T06.RecordUniqueNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class RecordUniqueNames
+{
+    public static void Main()
+    {
+        HashSet<string> names = new HashSet<string>();
+        int namesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < namesCount; i++)
+        {
+            string currName = Console.ReadLine();
+            names.Add(currName);
+        }
+
+        Console.WriteLine(String.Join(Environment.NewLine, names));
+    }
+}
