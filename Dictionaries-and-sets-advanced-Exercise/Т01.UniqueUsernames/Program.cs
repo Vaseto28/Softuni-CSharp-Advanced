b650e4ae65b77c1e99193ccc69436a20d5e7c89a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-09 13:59:13 +0300

    Completed task: T01.UniqueUsernames

---
+++ Dictionaries-and-sets-advanced-Exercise/Т01.UniqueUsernames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class UniqueUsernames
+{
+    public static void Main()
+    {
+        HashSet<string> usernames = new HashSet<string>();
+        int usernamesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < usernamesCount; i++)
+        {
+            string currUsername = Console.ReadLine();
+            usernames.Add(currUsername);
+        }
+
+        Console.WriteLine(String.Join(Environment.NewLine, usernames));
+    }
+}
