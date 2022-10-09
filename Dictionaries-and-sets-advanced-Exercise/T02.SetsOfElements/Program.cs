66ce2a2497c4397c39600f1c75fd75daec30b5e0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-09 13:59:31 +0300

    Completed task: T02.SetsOfElements

---
+++ Dictionaries-and-sets-advanced-Exercise/T02.SetsOfElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class SetsOfElements
+{
+    public static void Main()
+    {
+        int[] dimentionsForSets = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        List<int> firstList = new List<int>();
+        List<int> secondList = new List<int>();
+
+        for (int i = 0; i < dimentionsForSets[0]; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            if (!firstList.Contains(element))
+            {
+                firstList.Add(element);
+            }
+        }
+
+        for (int i = 0; i < dimentionsForSets[1]; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            if (!secondList.Contains(element))
+            {
+                secondList.Add(element);
+            }
+        }
+
+        List<int> occurances = new List<int>();
+        for (int i = 0; i < firstList.Count; i++)
+        {
+            int currElement = firstList[i];
+            if (secondList.Contains(currElement))
+            {
+                occurances.Add(currElement);
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", occurances));
+    }
+}
