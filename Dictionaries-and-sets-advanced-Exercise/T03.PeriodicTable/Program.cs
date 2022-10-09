481226ed4644641a5318028d7da37ae30d984b6a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-09 13:59:51 +0300

    Completed task: T03.PeriodicTable

---
+++ Dictionaries-and-sets-advanced-Exercise/T03.PeriodicTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class PeriodicTable
+{
+    public static void Main()
+    {
+        int chemicalsCount = int.Parse(Console.ReadLine());
+        HashSet<string> chemicals = new HashSet<string>();
+
+        for (int i = 0; i < chemicalsCount; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < input.Length; j++)
+            {
+                chemicals.Add(input[j]);
+            }
+        }
+
+        Console.WriteLine(String.Join(" ", chemicals.OrderBy(x => x)));
+    }
+}
