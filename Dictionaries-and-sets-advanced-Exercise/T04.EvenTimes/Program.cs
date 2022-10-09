135cd25db11fafb42b025a0fa4fee56e1a446d8f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-09 14:00:07 +0300

    Completed task: T04.EvenTimes

---
+++ Dictionaries-and-sets-advanced-Exercise/T04.EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class EvenTimes
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int currNum = int.Parse(Console.ReadLine());
+            if (!numbers.ContainsKey(currNum))
+            {
+                numbers.Add(currNum, 1);
+            }
+            numbers[currNum]++;
+
+        }
+
+        foreach (var kvp in numbers)
+        {
+            if (numbers[kvp.Key] % 2 != 0)
+            {
+                Console.WriteLine(kvp.Key);
+                break;
+            }
+        }
+    }
+}
