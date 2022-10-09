019704cced0ef60f50dd44c42611e4907d3cf861
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-09 14:00:22 +0300

    Completed task: T05.CountSymbols

---
+++ Dictionaries-and-sets-advanced-Exercise/T05.CountSymbols/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class CountSymbol
+{
+    public static void Main()
+    {
+        string inputText = Console.ReadLine();
+        Dictionary<char, int> occurances = new Dictionary<char, int>();
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            char currElement = inputText[i];
+            if (!occurances.ContainsKey(currElement))
+            {
+                occurances.Add(currElement, 0);
+            }
+            occurances[currElement]++;
+        }
+
+        foreach (var kvp in occurances.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+        }
+    }
+}
