0ccf4dab183e32e547fbdd60f75301557f7d8070
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-10 13:33:53 +0300

    Completed task: T02.LineNumbers

---
+++ Streams-files-and-directories-Lab/T02.LineNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"../../../input.txt";
+            string outputPath = @"../../../output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader sr = new StreamReader(inputFilePath);
+            using (sr)
+            {
+                StreamWriter sw = new StreamWriter(outputFilePath);
+                using (sw)
+                {
+                    int rowCounter = 1;
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        sw.WriteLine(rowCounter + ". " + line);
+                        rowCounter++;
+                    }
+                }
+            }
+        }
+    }
+}
