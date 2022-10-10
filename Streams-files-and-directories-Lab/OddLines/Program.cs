b3860f508b0c1c493f01e3d9af68149c20d511f4
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-10 13:33:34 +0300

    Completed task: T01.OddLines

---
+++ Streams-files-and-directories-Lab/OddLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../input.txt";
+            string outputFilePath = @"../../../output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader sr = new StreamReader(inputFilePath);
+            using (sr)
+            {
+                StreamWriter sw = new StreamWriter(outputFilePath);
+                using (sw)
+                {
+                    int counter = 0;
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            sw.WriteLine(line);
+                        }
+
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
