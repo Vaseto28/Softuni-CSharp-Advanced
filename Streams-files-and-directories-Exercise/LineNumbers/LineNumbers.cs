08edea7f65315268a7ffd5c1fbd10fcc7158790e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-11 13:06:54 +0300

    Completed task: T05.LineNumbers

---
+++ Streams-files-and-directories-Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../text.txt";
+            string outputFilePath = @"../../../output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            int lineCounter = 1;
+            foreach (var line in lines)
+            {
+                int letters = line.Count(char.IsLetter);
+                int punctuationMarks = line.Count(char.IsPunctuation);
+
+                Console.WriteLine($"Line {lineCounter}: {line} ({letters})({punctuationMarks})");
+            }
+        }
+    }
+}
