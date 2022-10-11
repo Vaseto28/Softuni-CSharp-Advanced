f1f6783ede440369c3dcfa8f89ef5f7c4f1fda89
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-11 13:06:31 +0300

    Completed task: T04.EvenLines

---
+++ Streams-files-and-directories-Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,48 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"../../../text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (counter % 2 == 0)
+                    {
+                        //replacing '-', ', ', '. ', '! ', '? ' with '@'
+                        line = line.Replace('-', '@')
+                            .Replace(',', '@')
+                            .Replace('.', '@')
+                            .Replace('!', '@')
+                            .Replace('?', '@');
+
+                        string[] processedLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        string[] finalLine = new string[processedLine.Length];
+                        for (int i = 0; i < processedLine.Length; i++)
+                        {
+                            finalLine[i] = processedLine[processedLine.Length - i - 1];
+                        }
+                        Console.WriteLine(String.Join(" ", finalLine));
+                    }
+
+                    counter++;
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+}
