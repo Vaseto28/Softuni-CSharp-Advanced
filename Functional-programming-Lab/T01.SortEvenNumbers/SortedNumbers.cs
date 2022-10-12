b0b25f01f4ced92297fc15d636b6b4cb8dc3aa3c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-12 14:01:59 +0300

    Completed task: T01.SortEvenNumbers

---
+++ Functional-programming-Lab/T01.SortEvenNumbers/SortedNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class SortedNumbers
+{
+    public static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        nums = nums.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+        Console.WriteLine(String.Join(", ", nums));
+    }
+}
