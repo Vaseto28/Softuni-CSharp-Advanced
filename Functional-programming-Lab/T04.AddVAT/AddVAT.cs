49a42aad1935f07be3760baeb22ba3e430b09fa1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-12 14:02:54 +0300

    Completed task: T04.AddVAT

---
+++ Functional-programming-Lab/T04.AddVAT/AddVAT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class AddVAT
+{
+    public static void Main()
+    {
+        decimal[] numbers = Console.ReadLine().Split(", ").Select(decimal.Parse).Select(num => num * 1.20m).ToArray();
+        foreach (var num in numbers)
+        {
+            Console.WriteLine($"{num:f2}");
+        }
+    }
+}
