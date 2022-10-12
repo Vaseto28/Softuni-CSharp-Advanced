39b5b83e464da654c7b1cab279a2140d3bdf4935
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-12 14:02:38 +0300

    Completed task: T03.CountUppercaseWords

---
+++ Functional-programming-Lab/T03.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class CountUppercaseWords
+{
+    public static void Main()
+    {
+        Func<string, bool> checker = word => word.Length > 0 && char.IsUpper(word[0]);
+
+        string[] words = Console.ReadLine().Split(' ').Where(w => checker(w)).ToArray();
+        Console.WriteLine(String.Join(Environment.NewLine, words));
+    }
+}
