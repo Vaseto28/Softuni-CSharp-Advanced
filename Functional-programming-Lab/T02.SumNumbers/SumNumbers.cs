57e5b325177f6ebae957ca486ab7c03fccbb776b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-12 14:02:20 +0300

    Completed task: T02.SumNumbers

---
+++ Functional-programming-Lab/T02.SumNumbers/SumNumbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class SuumNumbers
+{
+    public static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        Console.WriteLine(nums.Count() + Environment.NewLine + nums.Sum());
+    }
+}
