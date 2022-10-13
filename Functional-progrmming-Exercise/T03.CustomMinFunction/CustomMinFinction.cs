488f3deb0a9ada9e48599eff3fc9f8d59c640323
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-13 15:41:11 +0300

    Completed task: T03.CustomMinFunction

---
+++ Functional-progrmming-Exercise/T03.CustomMinFunction/CustomMinFinction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class CustomMinFinction
+{
+    public static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .OrderBy(x => x)
+            .ToArray();
+        Console.WriteLine(nums[0]);
+    }
+}
