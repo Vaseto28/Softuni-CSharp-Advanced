388416c92ab7ab7af4ba7297adf3e36089684331
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-13 15:42:06 +0300

    Completed task: T05.AppliedArithmetics

---
+++ Functional-progrmming-Exercise/T05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class AppliedArithmetics
+{
+    public static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        Func<int, int> add = x => x + 1;
+        Func<int, int> multiply = x => x * 2;
+        Func<int, int> subtract = x => x - 1;
+        Action<List<int>> print = x => Console.WriteLine(String.Join(" ", x));
+
+        string command;
+        while ((command = Console.ReadLine()) != "end")
+        {
+            if (command == "add")
+            {
+                numbers = numbers.Select(add).ToList();
+            }
+            else if (command == "multiply")
+            {
+                numbers = numbers.Select(multiply).ToList();
+            }
+            else if (command == "subtract")
+            {
+                numbers = numbers.Select(subtract).ToList();
+            }
+            else if (command == "print")
+            {
+                print(numbers);
+            }
+        }
+    }
+}
