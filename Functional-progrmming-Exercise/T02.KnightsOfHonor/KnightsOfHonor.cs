6f119f998cf0233384be08325cd89304e3b7e1f5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-13 15:40:54 +0300

    Completed task: T02.KnightsOfHonor

---
+++ Functional-progrmming-Exercise/T02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class KnightsOfHonor
+{
+    public static void Main()
+    {
+        string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Action<string> print = x => Console.WriteLine($"Sir {x}");
+        foreach (var name in names)
+            print(name);
+    }
+}
