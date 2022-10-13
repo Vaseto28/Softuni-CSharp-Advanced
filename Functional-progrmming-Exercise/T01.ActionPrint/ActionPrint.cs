70f1d7e811d5e523ca11b6b519db1095b5572391
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-13 15:40:38 +0300

    Completed task: T01.ActionPrint

---
+++ Functional-progrmming-Exercise/T01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class ActionPrint
+{
+    public static void Main()
+    {
+        string[] names = Console.ReadLine().Split(' ');
+        Action<string> print = x => Console.WriteLine(x);
+        foreach (var name in names)
+            print(name);
+    }
+}
