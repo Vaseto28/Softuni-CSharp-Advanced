ee5f8df2dc69c995ca0e1a7fe5b3120208dc5bfd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-14 12:04:57 +0300

    Completed task: T06.ReverseAndExcluse

---
+++ Functional-progrmming-Exercise/T06.ReverseAndExcluse/ReverseAndExcluse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class ReverseAndExcluse
+{
+    public static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Reverse()
+            .ToList();
+
+        int number = int.Parse(Console.ReadLine());
+        Predicate<int> checker = x => x % number != 0;
+
+        numbers = numbers.Where(x => checker(x)).ToList();
+
+        Console.WriteLine(String.Join(" ", numbers));
+    }
+}
