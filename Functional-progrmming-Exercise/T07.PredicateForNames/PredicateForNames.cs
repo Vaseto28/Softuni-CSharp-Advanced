0a6f5daafd54bcdbeedc8ca7f7d2134c21a59ba9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-14 12:05:57 +0300

    Completed task: T07.PredicateForNames

---
+++ Functional-progrmming-Exercise/T07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class PredicateForNames
+{
+    public static void Main()
+    {
+        int nameLength = int.Parse(Console.ReadLine());
+        List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        Predicate<string> checker = x => x.Length <= nameLength;
+        names = names.Where(x => checker(x)).ToList();
+        Console.WriteLine(String.Join(Environment.NewLine, names));
+    }
+}
