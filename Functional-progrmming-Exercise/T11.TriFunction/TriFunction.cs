e019279bd6d1e728cdf11403e1dc3192bf034dfd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-14 12:07:13 +0300

    Completed task: T11.TriFunction

---
+++ Functional-progrmming-Exercise/T11.TriFunction/TriFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class TriFunction
+{
+    public static void Main()
+    {
+        int sum = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split();
+
+        foreach (var name in names)
+        {
+            int currSum = 0;
+            foreach (var ch in name)
+            {
+                currSum += ch;
+            }
+
+            if (currSum >= sum)
+            {
+                Console.WriteLine(name);
+                break;
+            }
+        }
+    }
+}
