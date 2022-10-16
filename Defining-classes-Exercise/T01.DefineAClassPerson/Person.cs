f9d07b3bfdb6181a898b4303e3bf1416fe8f7894
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-16 17:59:26 +0300

    Completed task: T01.DefineAClassPerson

---
+++ Defining-classes-Exercise/T01.DefineAClassPerson/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
+
---
+++ Defining-classes-Exercise/T01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person newPerson = new Person();
+            newPerson.Name = "Vasil";
+            newPerson.Age = 15;
+        }
+    }
+}
+
