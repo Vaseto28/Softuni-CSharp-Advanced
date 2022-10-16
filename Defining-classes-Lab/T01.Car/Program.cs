9ccf33abfbefac956517d6fdd4e3812c6b75e639
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-16 17:58:02 +0300

    Completed task: T01.Car

---
+++ Defining-classes-Lab/T01.Car/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "Mercedes";
+            car.Model = "W140";
+            car.Year = 1996;
+
+            Console.WriteLine($"Make: {car.Make}{Environment.NewLine}Model: {car.Model}{Environment.NewLine}Year: {car.Year}");
+        }
+    }
+}
+
