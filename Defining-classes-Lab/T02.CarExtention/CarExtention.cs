57942ea2cc2998f679223e971dfbc88c24fb695d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-16 17:58:32 +0300

    Completed task: T02.CarExtention

---
+++ Defining-classes-Lab/T02.CarExtention/CarExtention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "Mercedes";
+            car.Model = "S580";
+            car.Year = 2021;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoIAm());
+        }
+    }
+}
+
