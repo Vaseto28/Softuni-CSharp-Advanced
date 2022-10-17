d5e8cf3fc3c9a09ad8d38ade39a47295ea6c0c61
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-17 19:33:04 +0300

    Completed task: T04.OpinionPoll

---
+++ Defining-classes-Exercise/T04.OpinionPoll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] properties = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = properties[0];
+                int age = int.Parse(properties[1]);
+                Person currPerson = new Person(name, age);
+
+                if (currPerson.Age > 30)
+                {
+                    people.Add(currPerson);
+                }
+            }
+
+            foreach (var person in people.OrderBy(x => x.Name).ToList())
+            {
+                Console.WriteLine(person.Name + " - " + person.Age);
+            }
+        }
+    }
+}
+
