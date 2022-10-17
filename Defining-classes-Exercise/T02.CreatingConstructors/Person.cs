6f970aa03491b9e575099da8286c01f4b151f152
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-17 19:32:10 +0300

    Completed task: T02.CreatingConstructors

---
+++ Defining-classes-Exercise/T02.CreatingConstructors/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        string name;
+        int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+        {
+            this.Name = "No name";
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
+
