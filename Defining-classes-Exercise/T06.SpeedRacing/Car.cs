75a9893f8d63a727ab89dc3fdaa12da0c95c63d5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-17 19:33:24 +0300

    Completed task: T06.SpeedRacing

---
+++ Defining-classes-Exercise/T06.SpeedRacing/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public decimal Fuel { get; set; }
+
+        public decimal FuelConsumptionPerKilometer { get; set; }
+
+        public decimal TravelledDistance { get; set; }
+
+        public Car(string model, decimal fuel, decimal consumption, decimal travelledDistance)
+        {
+            this.Model = model;
+            this.Fuel = fuel;
+            this.FuelConsumptionPerKilometer = consumption;
+            this.TravelledDistance = travelledDistance;
+        }
+
+        public bool IsItAbleToDriveTheDistance(decimal distance)
+        {
+            if (distance * this.FuelConsumptionPerKilometer <= this.Fuel)
+            {
+                this.Fuel -= distance * this.FuelConsumptionPerKilometer;
+                this.TravelledDistance += distance;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ Defining-classes-Exercise/T06.SpeedRacing/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = info[0];
+                decimal fuel = decimal.Parse(info[1]);
+                decimal fuelConsumption = decimal.Parse(info[2]);
+                decimal travelledDistance = 0;
+
+                Car newCar = new Car(model, fuel, fuelConsumption, travelledDistance);
+                cars.Add(newCar);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] info = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = info[1];
+                decimal distance = decimal.Parse(info[2]);
+
+                Car car = cars.Find(x => x.Model == model);
+                if (!car.IsItAbleToDriveTheDistance(distance))
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.Fuel:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
+
