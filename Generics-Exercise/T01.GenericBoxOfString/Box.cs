628aa2eefd534f2923f53a9a3f72cc7b634d5609
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-18 20:36:00 +0300

    Completed task: T01.GenericBoxOfString

---
+++ Generics-Exercise/T01.GenericBoxOfString/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public T Str { get; set; }
+
+        public Box(T str)
+        {
+            this.Str = str;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Str}";
+        }
+    }
+}
+
---
+++ Generics-Exercise/T01.GenericBoxOfString/GenericBoxOfString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int stringsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string currStr = Console.ReadLine();
+                Box<string> strBox = new Box<string>(currStr);
+                Console.WriteLine(strBox);
+            }
+        }
+    }
+}
