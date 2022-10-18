07a4a46953a8e323beb0dbc965c376f3e2ec2acc
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-18 20:36:49 +0300

    Completed task: T03.GenericSwapMethodString

---
+++ Generics-Exercise/T03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenericSwapMethodString
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            int stringsCount = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < stringsCount; i++)
+            {
+                string currStr = Console.ReadLine();
+                list.Add(currStr);
+            }
+
+            int[] indeces = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int firstIndex = indeces[0];
+            int secondIndex = indeces[1];
+            Box<string> newBox = new Box<string>(list);
+
+            newBox.Swapper(list, firstIndex, secondIndex);
+
+            foreach (string str in list)
+            {
+                Console.WriteLine($"{typeof(string)}: {str}");
+            }
+        }
+    }
+}
