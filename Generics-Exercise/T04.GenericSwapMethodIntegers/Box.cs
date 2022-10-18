5a7a512d2f33dc8c14293b16d8d7d7c8dfece437
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-18 20:37:12 +0300

    Completed task: T04.GenericSwapMethodIntegers

---
+++ Generics-Exercise/T04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericSwapMethodString
+{
+	public class Box<T>
+	{
+        public List<T> List { get; set; }
+
+        public Box(List<T> list)
+        {
+            this.List = list;
+        }
+
+        public void Swapper(List<T> list, int firstIndex, int secondIndex)
+        {
+            T firstElement = list[firstIndex];
+            T secondElement = list[secondIndex];
+
+            list[firstIndex] = secondElement;
+            list[secondIndex] = firstElement;
+        }
+    }
+}
+
---
+++ Generics-Exercise/T04.GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GenericSwapMethodString
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            int stringsCount = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < stringsCount; i++)
+            {
+                int currInteger = int.Parse(Console.ReadLine());
+                list.Add(currInteger);
+            }
+
+            int[] indeces = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int firstIndex = indeces[0];
+            int secondIndex = indeces[1];
+            Box<int> newBox = new Box<int>(list);
+
+            newBox.Swapper(list, firstIndex, secondIndex);
+
+            foreach (int currInteger in list)
+            {
+                Console.WriteLine($"{currInteger.GetType()}: {currInteger}");
+            }
+        }
+    }
+}
