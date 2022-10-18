d0f007d7cd2bd81a1534ad26b7a12139d437de89
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-18 20:36:20 +0300

    Completed task: T02.GenericBoxOfInteger

---
+++ Generics-Exercise/T02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        public T Int { get; set; }
+
+        public Box(T @int)
+        {
+            this.Int = @int;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.Int}";
+        }
+    }
+}
+
---
+++ Generics-Exercise/T02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int stringsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                int currInt = int.Parse(Console.ReadLine());
+                Box<int> boxOfIntegers = new Box<int>(currInt);
+                Console.WriteLine(boxOfIntegers);
+            }
+        }
+    }
+}
